2138eac7659aa9754adcc4edcecf23d637d35e8e
Fabrice <61694102+fabricewerger@users.noreply.github.com>
2020-10-06 18:42:10 +0200

    Update bot.py

--- code2/bot.py
+++ code2/bot.py
@@ -8,10 +8,10 @@ import random
 print('this is my twitterbot')
 
 #hiermee koppel ik de code met het twitteraccount
-CONSUMER_KEY = 'j0O27W3VkX5iO4SvKxxcS8Rpj'
-CONSUMER_SECRET = 'rgeGmawK1T7ysOcV0hgJNdaPLUW7XUpn7FzhvW3EoxAemNn4vD'
-ACCESS_KEY = '1259867690119954432-D5v7Sn1WBrrVkj3ULd9YGaDv7yWuWi'
-ACCESS_SECRET = 'emgQhuH7M42YUewgx6thLtAT1J294sHG3PHepCNo6JF0S'
+CONSUMER_KEY = '-'
+CONSUMER_SECRET = '-'
+ACCESS_KEY = '-'
+ACCESS_SECRET = '-'
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
